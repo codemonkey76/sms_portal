00e8531e96a142b6b9c8267934f52e6ec076ac5c
Shane Poppleton <shane@bjja.com.au>
2023-02-14 13:32:34 +1000

    Add EditList

--- app/Http/Livewire/Lists/EditListForm.php
+++ app/Http/Livewire/Lists/EditListForm.php
@@ -2,10 +2,41 @@
 
 namespace App\Http\Livewire\Lists;
 
+use App\Models\ContactList;
 use Livewire\Component;
 
 class EditListForm extends Component
 {
+    public ContactList $list;
+
+    protected function rules(): array
+    {
+        return [
+            'list.name' => 'required',
+            'list.customer_id' => 'required|exists:customers,id'
+        ];
+    }
+
+    public function mount(ContactList $list)
+    {
+        $this->list = $list;
+    }
+
+    public function back()
+    {
+        return redirect()->route('lists.index');
+    }
+
+    public function editList()
+    {
+        $this->validate();
+
+        $this->list->save();
+
+        return redirect()->route('lists.index');
+
+    }
+
     public function render()
     {
         return view('livewire.lists.edit-list-form');
