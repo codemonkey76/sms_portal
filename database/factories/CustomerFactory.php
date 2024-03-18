98998c358f678453cfbe0ca289cd27ae23498b9f
Shane Poppleton <shane@bjja.com.au>
2024-03-18 14:32:20 +1000

    Remove unecessary model prop

--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -2,18 +2,10 @@
 
 namespace Database\Factories;
 
-use App\Models\Customer;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class CustomerFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = Customer::class;
-
     /**
      * Define the model's default state.
      *
