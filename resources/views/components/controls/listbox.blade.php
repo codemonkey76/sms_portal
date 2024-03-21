4f32f17596949c7f674059d8a875353595f4d2d1
Shane Poppleton <shane@bjja.com.au>
2024-03-21 00:20:03 +0000

    Fixed prop

--- resources/views/components/controls/listbox.blade.php
+++ resources/views/components/controls/listbox.blade.php
@@ -1,33 +1,32 @@
 @props([
 "stacked" => true,
 "primary" => "name",
-"photo" => "profilePhotoUrl",
+"photo" => "profile_photo_url",
 "secondary" => "position",
 "category" => "letter",
 "list"
 ])
 <nav {{ $attributes->merge(['class' => 'h-full overflow-y-auto']) }} aria-label="Directory">
     @if ($stacked)
-        <div class="relative">
-            @foreach($list as $key => $group)
-                <div
-                    class="z-10 sticky top-0 border-t border-b border-gray-200 bg-gray-50 px-6 py-1 text-sm font-medium text-gray-500">
-                    <h3>{{$key}}</h3>
-                </div>
-                <ul class="relative z-0 divide-y divide-gray-200">
-                    @foreach($group as $item)
-                        <x-controls.listbox.item :item="$item" :photo="$photo" :primary="$primary" :secondary="$secondary"/>
-                    @endforeach
-                </ul>
+    <div class="relative">
+        @foreach($list as $key => $group)
+        <div class="z-10 sticky top-0 border-t border-b border-gray-200 bg-gray-50 px-6 py-1 text-sm font-medium text-gray-500">
+            <h3>{{$key}}</h3>
+        </div>
+        <ul class="relative z-0 divide-y divide-gray-200">
+            @foreach($group as $item)
+            <x-controls.listbox.item :item="$item" :photo="$photo" :primary="$primary" :secondary="$secondary" />
             @endforeach
-        </div>
+        </ul>
+        @endforeach
+    </div>
     @else
-        <div class="relative">
-            <ul class="relative z-0 divide-y divide-gray-200">
-                @foreach($list as $item)
-                    <x-controls.listbox.item :item="$item" :photo="$photo" :primary="$primary" :secondary="$secondary"/>
-                @endforeach
-            </ul>
-        </div>
+    <div class="relative">
+        <ul class="relative z-0 divide-y divide-gray-200">
+            @foreach($list as $item)
+            <x-controls.listbox.item :item="$item" :photo="$photo" :primary="$primary" :secondary="$secondary" />
+            @endforeach
+        </ul>
+    </div>
     @endif
 </nav>
