4886c746088f2a6de5b324ef3a0288ab931ff600
Shane Poppleton <shane@bjja.com.au>
2024-05-31 11:09:37 +1000

    Add telescope

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
-    App\Providers\TelescopeServiceProvider::class,
     App\Providers\JetstreamServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
 ];
